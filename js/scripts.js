ed7466dabd5d93db9e2770732803802cc5c37459
Olha Wysocky and Yulia Shidlovskaya <yulia@>
2019-01-15 14:29:09 -0800

    add ability to change background

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
+var isDark = false;
+
 $(document).ready(function(){
 
   $("#walrus-showing .clickable, #walrus-hidden .clickable").click(function(){
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
   $("#baby-showing .clickable, #baby-hidden .clickable").click(function(){
     $("#baby-showing").toggle();
     $("#baby-hidden").toggle();
@@ -25,7 +28,31 @@ $(document).ready(function(){
   $("#hide-seal, #show-seal").click(function(){
     $("#hide-seal").toggle();
     $("#show-seal").toggle();
-    $("#seal img").slideToggle(1000);
+    $("#seal img").fadeToggle(1000);
   });
+
+  $("#btn-theme").click(function(){
+    if(isDark){
+      $("body").removeClass("dark-background");
+      $("body").addClass("light-background");
+      $("button").removeClass("dark");
+      $("button").addClass("light");
+      isDark = false;
+    }else{
+      $("body").removeClass("light-background");
+      $("body").addClass("dark-background");
+      $("button").removeClass("light");
+      $("button").addClass("dark");
+      isDark = true;
+    }
+  });
+
+  // $(".light").click(function(){
+  //   $("body").removeClass("light-background");
+  //   $("body").addClass("dark-background");
+  //   $("button").removeClass("light");
+  //   $("button").addClass("dark");
+  //   alert("Light");
+  // });
 
 });
